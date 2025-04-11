d1ef78d6cb6163ed0b35a80527cf8cf0325c29fc
mordz <mordz@email.cz>
2025-04-11 07:10:50 +0200

    v

--- s2.asm
+++ s2.asm
@@ -25954,31 +25954,31 @@ Ani_obj26_Static:
 	dc.b	$FF	; terminator
 ; byte_12CE8:
 Ani_obj26_Sonic:
-	dc.b   1,  0,  2,  2,  1,  2,  2,$FF
+	dc.b   1,  2,  2,,$FF
 ; byte_12CF0:
 Ani_obj26_Tails:
-	dc.b   1,  0,  3,  3,  1,  3,  3,$FF
+	dc.b   1,  3,  3,$FF
 ; byte_12CF8:
 Ani_obj26_Eggman:
-	dc.b   1,  0,  4,  4,  1,  4,  4,$FF
+	dc.b   1,  4,  4,$FF
 ; byte_12D00:
 Ani_obj26_Ring:
-	dc.b   1,  0,  5,  5,  1,  5,  5,$FF
+	dc.b   1,  5,  5, $FF
 ; byte_12D08:
 Ani_obj26_Shoes:
-	dc.b   1,  0,  6,  6,  1,  6,  6,$FF
+	dc.b   1,  6,  6,  $FF
 ; byte_12D10:
 Ani_obj26_Shield:
-	dc.b   1,  0,  7,  7,  1,  7,  7,$FF
+	dc.b   1,  7,  7, $FF
 ; byte_12D18:
 Ani_obj26_Invincibility:
-	dc.b   1,  0,  8,  8,  1,  8,  8,$FF
+	dc.b   1,  8,  8,$FF
 ; byte_12D20:
 Ani_obj26_Teleport:
-	dc.b   1,  0,  9,  9,  1,  9,  9,$FF
+	dc.b   1,  9,  9,$FF
 ; byte_12D28:
 Ani_obj26_QuestionMark:
-	dc.b   1,  0, $A, $A,  1, $A, $A,$FF
+	dc.b   1,  $A, $A, $FF
 ; byte_12D30:
 Ani_obj26_Broken:
 	dc.b   2,  0,  1, $B,$FE,  1
